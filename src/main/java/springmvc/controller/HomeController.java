0cc94485adf712a1dd34e9fd8e312a8b11f0546e
cqian59420-Home-PC <cqian59420@gmail.com>
2014-12-10 22:10:07 +0800

    commit file 1

--- src/main/java/springmvc/controller/HomeController.java
+++ src/main/java/springmvc/controller/HomeController.java
@@ -2,18 +2,14 @@ package springmvc.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.aop.config.AbstractInterceptorDrivenBeanDefinitionDecorator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.portlet.ModelAndView;
 import springmvc.service.IHomeService;
 
 import javax.annotation.Resource;
-import javax.inject.Inject;
-import java.util.Map;
 
 @Controller
 @RequestMapping("/homeController.do")
@@ -24,21 +20,20 @@ public class HomeController {
     private IHomeService homeService;
 
     @RequestMapping("login")
-    public ModelAndView login(@RequestParam("userName") String name,@RequestParam("password") String passworld,
+    public String login(@RequestParam("userName") String name,@RequestParam("password") String passworld,
     Model model){
         logger.info("成功调用到login方法{},密码{}",name,passworld);
-       ModelAndView modelAndView = new ModelAndView();
-
-    /*    model.addAttribute("name",name);
+       /*ModelAndView modelAndView = new ModelAndView();*/
+        model.addAttribute("name",name);
         model.addAttribute("passworld",passworld);
         if(homeService.login(name,passworld)){
             return  "home/index";
         }else {
             return "home/error";
-        }*/
-        modelAndView.addObject("name",name);
+        }
+       /* modelAndView.addObject("name",name);
         modelAndView.addObject("passworld",passworld);
         modelAndView.setViewName("index");
-        return modelAndView;
+        return modelAndView;*/
     }
 }
